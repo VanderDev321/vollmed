de745be41fe772e34e33d040ced98ca648e7e115
vanderson teles <vander.drums@hotmail.com>
2025-02-01 10:54:56 -0300

    "criação de interface intermediária para paginação"

---
+++ src/app/interfaces/pageavel.ts
@@ -0,0 +1,14 @@
+export interface Pageable{
+  nome:string,
+  cpf: string,
+  crm:string,
+  email:string,
+  telefone: string,
+  cep: string,
+  logradouro: string,
+  bairro: string,
+  numero: string,
+  estado: string,
+  complemento?: string,
+  especialidade:string,
+}
